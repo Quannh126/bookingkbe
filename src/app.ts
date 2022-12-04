d5015fe882b206cc1ae6492bbb021dd739a90180
Quan <quan.nh148983@sis.hust.edu.vn>
2022-12-05 01:16:35 +0700

    connect Db

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import {Route} from 'core/interfaces'
+import mongoose from 'mongoose';
 class App {
     public app: express.Application;
     public port: string | number;
@@ -7,6 +8,7 @@ class App {
         this.app = express();
         this.port = process.env.PORT || 5000;
         this.initializeRoutes(routes);
+        this.connectToDatabase();
     }
     public listen(){
         this.app.listen(this.port, ()=>{
@@ -19,7 +21,20 @@ class App {
         })
     }
 
-    
+    private connectToDatabase(){
+        try {
+            const connectString = process.env.MONGODB_URL;
+            
+            if(!connectString){
+                console.log('Connectiong string is blank');
+                return;
+            }
+            mongoose.connect(connectString);
+            console.log('Database connected...')
+        } catch (error) {
+            console.log("Connect to database error")
+        }
+    }
 }
 
 export default App;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
+import "dotenv/config";
 import App from "./app";
 import {IndexRoute} from './modules/index';
+require('dotenv').config()
 const routes = [new IndexRoute()]
 const app = new App(routes);
 
