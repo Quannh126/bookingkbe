d06e059ffbaad8d34047dc11536192189990dcf9
Quan <quan.nh148983@sis.hust.edu.vn>
2022-12-05 10:44:15 +0700

    Add and setup some Middlewares package

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,25 @@
 import express from 'express'
 import {Route} from 'core/interfaces'
 import mongoose from 'mongoose';
+import hpp from 'hpp';
+import helmet from 'helmet'
+import cors from 'cors';
+import morgan from 'morgan'
+import { Logger } from './core/utils';
 class App {
     public app: express.Application;
     public port: string | number;
+    public production: boolean;
     constructor(routes: Route[]){
         this.app = express();
         this.port = process.env.PORT || 5000;
+        this.production = process.env.NODE_ENV == 'production'? true : false;
         this.initializeRoutes(routes);
         this.connectToDatabase();
     }
     public listen(){
         this.app.listen(this.port, ()=>{
-            console.log(`Server is listening on port ${this.port}`);
+            Logger.info(`Server is listening on port ${this.port}`);
         });
     }
     private initializeRoutes(routes: Route[]){
@@ -22,18 +29,37 @@ class App {
     }
 
     private connectToDatabase(){
-        try {
+        
             const connectString = process.env.MONGODB_URL;
-            
+            const options = {
+
+            };
             if(!connectString){
-                console.log('Connectiong string is blank');
+                Logger.info('Connectiong string is blank');
                 return;
             }
-            mongoose.connect(connectString);
-            console.log('Database connected...')
-        } catch (error) {
-            console.log("Connect to database error")
+            mongoose
+                .connect(connectString).catch((error)=>{
+                    Logger.error(error);
+            })
+            Logger.info('Database connected...')
+
+        
+    }
+
+    private initializeMiddleWares(){
+        if(this.production){
+            this.app.use(hpp());
+            this.app.use(helmet());
+            this.app.use(cors({origin: 'https://booking-k.vercel.app/', credentials: true}));
+            this.app.use(morgan('combinded'));
+            
+
+        }else{
+            this.app.use(cors({origin: true, credentials: true}));
+            this.app.use(morgan('dev'));
         }
+
     }
 }
 
